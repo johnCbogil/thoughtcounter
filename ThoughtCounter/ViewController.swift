78140a8826833f33c41bee92e93f586cb411ddfa
John Bogil <johncbogil@gmail.com>
2018-06-08 16:57:53 -0400

    Initial Commit

---
+++ ThoughtCounter/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ThoughtCounter
+//
+//  Created by John Bogil on 6/8/18.
+//  Copyright © 2018 John Bogil. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ ThoughtCounterTests/ThoughtCounterTests.swift
@@ -0,0 +1,36 @@
+//
+//  ThoughtCounterTests.swift
+//  ThoughtCounterTests
+//
+//  Created by John Bogil on 6/8/18.
+//  Copyright © 2018 John Bogil. All rights reserved.
+//
+
+import XCTest
+@testable import ThoughtCounter
+
+class ThoughtCounterTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
