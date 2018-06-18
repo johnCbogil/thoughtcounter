b4be88c335534c17c839a3a6d207a0a39c437c8a
John Bogil <johncbogil@gmail.com>
2018-06-18 18:32:21 -0400

    got some lorem ipsum text

--- ThoughtCounter/Controller/OnboardingViewController.swift
+++ ThoughtCounter/Controller/OnboardingViewController.swift
@@ -15,6 +15,8 @@ class OnboardingViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        label.text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
+        
         // Do any additional setup after loading the view.
     }
     @IBAction func getStarted(_ sender: Any) {
