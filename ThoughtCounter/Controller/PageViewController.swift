4cd5136531d088e3cbdd8e9bd465ca44e7d07613
John Bogil <johncbogil@gmail.com>
2018-06-24 21:45:18 -0400

    The pageVC looks ready to go, just need to hook it up

--- ThoughtCounter/Controller/PageViewController.swift
+++ ThoughtCounter/Controller/PageViewController.swift
@@ -10,6 +10,10 @@ import UIKit
 
 class PageViewController: UIPageViewController, UIPageViewControllerDelegate, UIPageViewControllerDataSource {
     
+    var listOfViewControllers = [OnboardingViewController]()
+    let questionsArray = ["What is an intrusive thought?", "What can I do to treat intrusive thoughts?", "How does counting help?" ]
+    let answersArray = ["An intrusive thought is an unwelcome involuntary thought, image, or unpleasant idea that can feel difficult to manage or eliminate.","One type of treatment is to monitor these thoughts by counting them.","Self-monitoring is proven to help develop self-control and decrease intrusive thoughts. At first you will notice that the number increases; this will happen for several days as you get better at identifying intrusive thoughts. Soon you will see that the daily total plateaus after 7 - 10 days, followed by a decrease in intrusive thoguhts."]
+
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -21,13 +25,47 @@ class PageViewController: UIPageViewController, UIPageViewControllerDelegate, UI
         vc2.index = 1
         let vc3 = storyboard?.instantiateViewController(withIdentifier: "OnboardingViewController") as! OnboardingViewController
         vc3.index = 2
+        listOfViewControllers.append(contentsOf: [vc1,vc2,vc3])
+        
+//        self.setViewControllers([listOfViewControllers[0]] as [OnboardingViewController], direction: UIPageViewControllerNavigationDirection.forward, animated: false, completion: nil)
+        self.setViewControllers([getViewControllerAtIndex(index: 0)] as [UIViewController], direction: UIPageViewControllerNavigationDirection.forward, animated: false, completion: nil)
+
+        
     }
     
     func pageViewController(_ pageViewController: UIPageViewController, viewControllerBefore viewController: UIViewController) -> UIViewController? {
-        <#code#>
+        let pageContent: OnboardingViewController = viewController as! OnboardingViewController
+        var index = pageContent.index
+        if ((index == 0) || (index == NSNotFound))
+        {
+            return nil
+        }
+        index -= 1
+        return getViewControllerAtIndex(index: index)
     }
     
     func pageViewController(_ pageViewController: UIPageViewController, viewControllerAfter viewController: UIViewController) -> UIViewController? {
-        <#code#>
+        let pageContent: OnboardingViewController = viewController as! OnboardingViewController
+        var index = pageContent.index
+        if (index == NSNotFound)
+        {
+            return nil;
+        }
+        
+        index += 1
+        if (index == questionsArray.count)
+        {
+            return nil;
+        }
+        return getViewControllerAtIndex(index: index)
+    }
+    
+    func getViewControllerAtIndex(index: NSInteger) -> OnboardingViewController {
+        // Create a new view controller and pass suitable data.
+        let pageContentViewController = self.storyboard?.instantiateViewController(withIdentifier: "PageContentViewController") as! OnboardingViewController
+        pageContentViewController.questionLabel.text = "\(questionsArray[index])"
+        pageContentViewController.answerLabel.text = "\(answersArray[index])"
+        pageContentViewController.index = index
+        return pageContentViewController
     }
 }
